98a325f583ff81158444068c09409783f1ad5628
Kauan Gomes <Kauang13@gmail.com>
2023-04-18 09:32:12 -0300

    botao de visibilidade de carrosel adicionado

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,88 @@
-function showHide(id) {
-    let conteudo =document.querySelector(id);
-    conteudo.classList.toggle('ativo')
-}
+
+    $("#time").click(function(){
+        $("#exemplo1").toggle();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#infantil").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").toggle();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#princesas").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").toggle();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#profissões").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").toggle();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#adultos").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").toggle();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#anime").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").show();
+        $("#exemplo7").hide();
+        $("#exemplo8").hide();
+    });
+
+    $("#florks").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").toggle();
+        $("#exemplo8").hide();
+    });
+
+    $("#super-heroi").click(function(){
+        $("#exemplo1").hide();
+        $("#exemplo2").hide();
+        $("#exemplo3").hide();
+        $("#exemplo4").hide();
+        $("#exemplo5").hide();
+        $("#exemplo6").hide();
+        $("#exemplo7").hide();
+        $("#exemplo8").toggle();
+    });
